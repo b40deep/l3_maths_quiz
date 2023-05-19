fd2ee3c9ef3eaa82b674b484a66703c34f61ae77
boydee thinkpad <migisb@gloscol.ac.uk>
2023-05-19 11:45:50 +0100

    did some bug fixes

--- js/index.js
+++ js/index.js
@@ -4,15 +4,20 @@ function testJS() {
 
 let username = "";
 let highScore = 0;
-const inputElement = document.getElementById("input-name");
+const inputElement = document.getElementById("input_name");
 const highscoreSpan = document.querySelector("#highscore");
 
+loadDefaults();
+
 function saveName() {
-  let inputValue = inputElement.value;
-  username = inputValue;
-  console.log("input-username:", inputValue);
-  // alert("javascript said your username is " + inputValue + "!");
-  localStorage.setItem("username", inputValue);
+  username = inputElement.value;
+  // console.log("input-username:", inputValue);
+  // alert(`javascript said your username is ${username}!`);
+  if (username !== undefined && username !== null && username.length > 0) {
+    localStorage.setItem("username", username);
+    // alert(`javascript said your username is ${username.length}!`);
+    loadDefaults();
+  }
 
   // send data to the service worker if one exists
   // if ("serviceWorker" in navigator) {
@@ -20,18 +25,17 @@ function saveName() {
   //     "Hello from the web page!" + inputValue
   //   );
   // }
-  highscoreSpan.textContent =
-    username + " got " + localStorage.getItem("highscore") ||
-    "failed to load highscore";
 }
 
 // const testJSbtn = document.querySelector("#test-js-button");
 // testJSbtn.addEventListener("click", testJS);
 
-//check if highscore is available and display it
-username = localStorage.getItem("username");
-highScore = localStorage.getItem("highscore");
-highscoreSpan.textContent =
-  (username == undefined ? "Friend" : username) +
-  ", your high score is " +
-  (highScore == null ? "yet to be set" : highScore);
+function loadDefaults() {
+  //check if highscore is available and display it
+  username = localStorage.getItem("username");
+  highScore = localStorage.getItem("highscore");
+  highscoreSpan.textContent =
+    (username == undefined ? "Friend" : username) +
+    ", your high score is " +
+    (highScore == null ? "yet to be set" : highScore);
+}
