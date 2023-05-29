a6aa8c8fe58767d520d0264c090bb45be0a7f909
boydee thinkpad <migisb@gloscol.ac.uk>
2023-05-29 16:35:34 +0100

    added all files to service worker cache list

--- sw.js
+++ sw.js
@@ -25,17 +25,17 @@ self.addEventListener("fetch", function (event) {
   );
 });
 
-//receive messages from the website
+//receive messages from the website - you'd use this to get data straight from your pages
 self.addEventListener("message", function (event) {
   data = event.data;
   // console.log("Received message from web page:", data);
-  console.log("data:", data["username"]);
+  // console.log("data:", data["username"]);
   // caches.open("static-v1").then(function (cache) {
   //   return cache.put("username", data["username"]);
   // });
 });
 
-//cache the static assets
+//cache the static assets so you app can work offline
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("static-v1").then(function (cache) {
@@ -47,7 +47,36 @@ self.addEventListener("install", function (event) {
         "/images/logo256.png",
         "/images/logo512.png",
         "/images/logo96.png",
+        "/images/game/-.png",
+        "/images/game/+.png",
+        "/images/game/d.png",
+        "/images/game/x.png",
+        "/images/game/1.png",
+        "/images/game/2.png",
+        "/images/game/3.png",
+        "/images/game/4.png",
+        "/images/game/5.png",
+        "/images/game/6.png",
+        "/images/game/7.png",
+        "/images/game/8.png",
+        "/images/game/9.png",
+        "/images/game/10.png",
+        "/images/game/11.png",
+        "/images/game/12.png",
+        "/images/game/bg.png",
+        "/images/game/fail1.png",
+        "/images/game/fail2.png",
+        "/images/game/fail3.png",
+        "/images/game/fail4.png",
+        "/images/game/pass1.png",
+        "/images/game/pass2.png",
+        "/images/game/pass3.png",
+        "/images/game/pass4.png",
+        "/images/game/temp.png",
+        "/images/game/bgSoundtrue.png",
+        "/images/game/bgSoundfalse.png",
         "/css/bootstrap.min.css",
+        "/css/custom.css",
         "/js/bootstrap.min.js",
         "/js/camera.js",
         "/js/index.js",
@@ -55,6 +84,8 @@ self.addEventListener("install", function (event) {
         "/js/sound.js",
         "/sound/fail.mp3",
         "/sound/pass.mp3",
+        "/sound/bg.mp3",
+        "/font/comic.ttf",
       ]);
     })
   );
